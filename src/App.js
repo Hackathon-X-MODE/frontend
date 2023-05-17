51f36935d8fae0dd44dba7298cb926a9f1eae265
Roseskk <oleg419583@yandex.ru>
2023-05-17 23:59:13 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
      <div className={'max-w-[1080px] mx-auto'}>
-        <h1 className={'font-bold'}>POSTAMAT APP</h1>
-
+         <h1 className={'font-bold'}>POSTAMAT APP</h1>
+         <span className={'test'}>test</span>
      </div>
   );
 }
