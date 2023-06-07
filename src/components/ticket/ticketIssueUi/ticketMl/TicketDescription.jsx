cf424d3e3e8151b0382121899e7a3d77082bfeee
Roseskk <oleg419583@yandex.ru>
2023-06-07 15:06:35 +0300

    refactoring COMMENTARY

--- src/components/ticket/ticketIssueUi/ticketMl/TicketDescription.jsx
+++ src/components/ticket/ticketIssueUi/ticketMl/TicketDescription.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import TicketDescriptionHeader from "./TicketDescriptionHeader";
 import TicketComment from "./TicketComment";
 
-const TicketDescription = ({ticket, order, comments}) => {
+const TicketDescription = ({ticket, order, comments, selectHandler}) => {
+    // console.log('hw',comments)
     return(
         <div className={'w-full flex flex-col  bg-[#5C5F7E] rounded-[15px] font-primary text-white text-[18px]'}>
             <div className={'px-[30px] py-[20px] flex items-center justify-between text-white font-primary mr-[77px]'}>
@@ -12,7 +13,7 @@ const TicketDescription = ({ticket, order, comments}) => {
                 ticket &&
                 <div className={'w-full px-[30px] py-[40px] bg-[#21243A] flex flex-col gap-[20px] items-center justify-between text-white font-primary mr-[77px] rounded-bl-[15px] rounded-br-[15px]'}>
                     {
-                        comments?.map(comment => <TicketComment key={comment.id} ticket={ticket} order={order} comment={comment} />)
+                        comments?.map(comment => <TicketComment key={comment.id} ticket={ticket} order={order} comment={comment} selectHandler={selectHandler} />)
                     }
                 </div>
             }
--- src/components/ticket/ticketIssueUi/ticketMl/category/Editor.jsx
+++ src/components/ticket/ticketIssueUi/ticketMl/category/Editor.jsx
@@ -14,7 +14,71 @@ const reactSelectValues = [
     {value: 'PREPARE_ORDER', label: 'Оформление заказа'},
 ]
 
-const Editor = ({comment}) => {
+const map = {
+    "PRODUCT_DESCRIPTION": [],
+    "PREPARE_ORDER": [
+        "SELECT_POSTAMAT",
+        "SEARCH_POSTAMAT_AT_HOUSE"
+    ],
+    "GETTING_ORDER": [
+        "PAY_ORDER",
+        "OPEN_POSTAMAT"
+    ],
+    "GOT_ORDER": [
+        "PACKING",
+        "COMPLETENESS"
+    ],
+    "PRODUCT": [
+        "QUALITY",
+        "DESCRIPTION"
+    ],
+    "POST_BOX": [
+        "WORK_POSTAMAT",
+        "LOCATION_POSTAMAT",
+        "VIEW_POSTAMAT"
+    ],
+    "DELIVERY": [
+        "DEADLINE",
+        "COAST_DELIVERY",
+        "DELIVERY_GUY_REPORT"
+    ],
+    "NOTIFICATION": [
+        "CONFIRM_NOTIFICATION",
+        "DELIVERY_NOTIFICATION",
+        "READY_NOTIFICATION"
+    ],
+    "OTHER": []
+}
+
+const Editor = ({comment, selectHandler, references}) => {
+    const reference = {
+        PRODUCT_DESCRIPTION: 'Описание товара на сайте интернет-магазина',
+        GETTING_ORDER: 'Получение заказа',
+        GOT_ORDER: 'Полученный заказ',
+        PRODUCT: 'Товар',
+        POST_BOX: 'Работа постамата',
+        DELIVERY: 'Доставка',
+        NOTIFICATION: 'Уведомления',
+        OTHER: 'Иное',
+        PREPARE_ORDER: 'Оформление заказа',
+        SELECT_POSTAMAT: 'Выбор способа доставки в постамат',
+        SEARCH_POSTAMAT_AT_HOUSE: 'Поиск постамата в конкретном подъезде дома',
+        PAY_ORDER: 'Оплата заказа',
+        OPEN_POSTAMAT: 'Открытие ячейки',
+        PACKING: 'Упаковка',
+        COMPLETENESS: 'Комплектность',
+        QUALITY: 'Качество',
+        DESCRIPTION: 'Несоответствие описанию на сайте',
+        WORK_POSTAMAT: 'Работа постамата',
+        LOCATION_POSTAMAT: 'Местоположение постамата',
+        VIEW_POSTAMAT: 'Внешний вид постамата',
+        DEADLINE: 'Сроки доставки',
+        COAST_DELIVERY: 'Стоимость доставки',
+        DELIVERY_GUY_REPORT: 'Жалоба на работу курьеров',
+        CONFIRM_NOTIFICATION: 'Уведомление об оформленном заказе',
+        DELIVERY_NOTIFICATION: 'Уведомление о дате доставки',
+        READY_NOTIFICATION: 'Уведомление о готовности заказа к получению'
+    }
     const customStyles = {
         option: (defaultStyles, state) => ({
             ...defaultStyles,
@@ -26,9 +90,9 @@ const Editor = ({comment}) => {
             ...defaultStyles,
 
             backgroundColor: "#5C5F7E",
-            height: '5px',
+            height: '50px',
             borderRadius: "15px",
-            overflow: 'hidden',
+            overflowY: 'scroll',
             border: "1px solid #5C5F7E",
             boxShadow: "none",
         }),
@@ -37,7 +101,24 @@ const Editor = ({comment}) => {
     const reactSelectSingleDefaultOption = comment.commentTypes.map((commentTypeName) => {
         return reactSelectValues.find((category) => commentTypeName.name === category.value)
     })
-    console.log(reactSelectSingleDefaultOption)
+    const reactMultyDefaultOption = comment.commentTypes.map((commentTypeName) => {
+        return commentTypeName.value.map(name => {
+            return{
+                value: name,
+                label: reference[name]
+            }
+        })
+    })
+
+    const defaultTypes  = comment.commentTypes.map(type => map[type.name])
+    const defTypes = defaultTypes.map((item) => {
+        return  item.map(name => {
+            return {
+                value: name,
+                label: reference[name]
+            }
+        })})
+
     return(
         <div className={'w-full  flex p-2 text-white font-primary pl-[120px]'}>
             <div className={'w-4/12 flex flex-col'}>
@@ -54,33 +135,33 @@ const Editor = ({comment}) => {
                                     }}
                                     defaultValue={item}
                                     styles={customStyles}
+                                    onChange={(e) => selectHandler(e, comment.id, item, 'single')}
                                 />
                             </div>
                         )
                     })
                 }
-                {/*{*/}
-                {/*    comment.commentTypes.map((category) => {*/}
-                {/*        return(*/}
-                {/*            <div  className={'flex flex-col mt-[10px] border-b border-b-gray-500'}>*/}
-                {/*                <span className={'text-[#6C7094]'}>Категория</span>*/}
-                {/*                <span>{reference[category.name]}</span>*/}
-                {/*            </div>*/}
-                {/*        )*/}
-                {/*    })*/}
-                {/*}*/}
             </div>
             <div className={'w-8/12'}>
-                {/*{*/}
-                {/*    comment.commentTypes.map((category) => {*/}
-                {/*        return(*/}
-                {/*            <div  className={'w-full flex flex-col mt-[10px] border-b border-b-gray-500'}>*/}
-                {/*                <span className={'text-[#6C7094]'}>Подкатегория</span>*/}
-                {/*                <span>{category.value.length === 0 ? '-' : category.value.map(v => reference[v]).join(', ')}</span>*/}
-                {/*            </div>*/}
-                {/*        )*/}
-                {/*    })*/}
-                {/*}*/}
+                {
+                    reactMultyDefaultOption.map((item,idx) => {
+                        return(
+                            <div key={`${item.value}_${idx}`}   className={'flex flex-col mt-[10px] '}>
+                                <span className={'text-[#6C7094]'}>Подкатегория</span>
+                                <ReactSelect
+                                    isMulti={true}
+                                    options={defTypes[idx]}
+                                    components={{
+                                        Option
+                                    }}
+                                    defaultValue={item}
+                                    styles={customStyles}
+                                    onChange={(e) => selectHandler(e, comment.id, item)}
+                                />
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
