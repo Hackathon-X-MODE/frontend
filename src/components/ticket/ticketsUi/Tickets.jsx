ad79ae8f57967c15b9abc10ba22859fe21cd249a
Alexander Chapchuk <BigTows@yandex.ru>
2023-06-10 19:17:35 +0300

    Add pagination to tickets.

--- src/components/ticket/ticketsUi/Tickets.jsx
+++ src/components/ticket/ticketsUi/Tickets.jsx
@@ -1,52 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import {
-    useGetTicketsQuery,
-} from "../../../redux/postamatApi";
+import React, {useEffect, useState} from 'react';
+import {useGetTicketsQuery,} from "../../../redux/postamatApi";
 import Loader from "../../loader/Loader";
 import TicketHeader from "./ticketHeader/TicketHeader";
 import TicketBody from "./ticketBody/TicketBody";
-import TicketsPagination from "./TicketsPagination";
 import TicketsStatus from "./TicketsStatus";
+import ReactPaginate from "react-paginate";
 
 
 const Tickets = (props) => {
     const [initialData, setInitialData] = useState()
     const [page, setPage] = useState(0)
+    const [pages, setPages] = useState(0)
     const [status, setStatus] = useState('OPEN')
 
-    const { data: allTickets = [], isFetching } = useGetTicketsQuery({page: page, status: status})
+    const {data: allTickets = [], isFetching} = useGetTicketsQuery({page: page, status: status})
 
-    useEffect(()=> {
+    useEffect(() => {
+        if (!allTickets || allTickets.length === 0) {
+            return
+        }
         setInitialData(allTickets)
+        setPages([...Array(allTickets[0].totalPages)])
     }, [isFetching, page, status])
 
-    const handlePage = (type) => {
-        if (type === 'next' && initialData[0].content.length === 20) {
-            setPage((prevPage) => prevPage + 1)
-        } else if( type === 'prev' && page !== 0) {
-            setPage((prevPage) => prevPage - 1)
-        }
-    }
+    const pageCount = Math.max(Math.ceil(pages.length / 20), 1);
 
     const handleFilter = (e) => {
         setStatus(e.target.name)
         setPage(0)
     }
 
-    if (!initialData) return  <Loader />
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * 20) % pages.length;
+        const target = event.selected
+        setPage(target)
+    }
 
-    if (isFetching) return <Loader />
+    if (!initialData) return <Loader/>
 
-    return(
+    if (isFetching) return <Loader/>
+
+    return (
         <div className={'w-full relative  flex flex-col  mt-[50px] font-primary'}>
-            <div className={'mx-[70px] w-5/6 px-[70px] flex flex-col h-[85vh]  flex flex bg-[#21243A] rounded-[15px] overflow-y-scroll rounded-tr-[0px]'}>
-                <TicketHeader />
+            <div
+                className={'mx-[70px] w-5/6 px-[70px] flex flex-col h-[85vh]  flex flex bg-[#21243A] rounded-[15px] overflow-y-scroll rounded-tr-[0px]'}>
+                <TicketHeader total={allTickets[0]?.totalElements}/>
                 {
-                    initialData.length !== 0 && <TicketBody tickets={initialData} />
+                    initialData.length !== 0 && <TicketBody tickets={initialData}/>
                 }
-                <TicketsStatus status={status} handleFilter={handleFilter} />
+                <TicketsStatus status={status} handleFilter={handleFilter}/>
             </div>
-            <TicketsPagination pageHandler={handlePage} />
+            <ReactPaginate
+                className={'flex items-center justify-end -translate-x-[200px] text-white  gap-[5px] mt-[20px]'}
+                breakLabel="..."
+                nextLabel=">"
+                nextLinkClassName={'bg-[#21243A] px-[10px] py-[10px] rounded-[10px]'}
+                previousClassName={'bg-[#21243A] px-[10px] py-[10px] rounded-[10px]'}
+                onPageChange={handlePageClick}
+                initialPage={page}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                activeLinkClassName={'bg-[#5C5F7E]'}
+                previousLabel="<"
+                pageLinkClassName={'px-[10px] py-[10px] text-white rounded-[10px]'}
+            />
         </div>
     )
 }
--- src/components/ticket/ticketsUi/ticketHeader/TicketHeader.jsx
+++ src/components/ticket/ticketsUi/ticketHeader/TicketHeader.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const TicketHeader = (props) => {
+const TicketHeader = ({total}) => {
     return(
         <div className={'sticky top-0 bg-[#21243A] z-10 flex w-full justify-around  gap-2  text-white text-[18px] px-[30px] py-[24px]'}>
+            <div className={'translate-x-[70px] text-white'}>Всего обращений: {total}</div>
              <div className={'w-1/4 text-center'}>Номер</div>
              <div className={'w-1/4 text-center'}>Вендор</div>
              <div className={'w-1/4 text-center'}>Дата обращения</div>
